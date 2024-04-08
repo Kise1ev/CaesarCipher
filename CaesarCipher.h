266cc52811f52e45ba1534663492fd5e4cbe199f
Kiselev <kiselev.legend@gmail.com>
2024-04-09 00:19:38 +0300

    Add project files.

---
+++ CaesarCipher.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+string encryptString(const string& text, int shift);
---
+++ Main.cpp
@@ -0,0 +1,29 @@
+﻿#include "CaesarCipher.h"
+
+string encryptString(const string& text, int shift) {
+    string encryptedString;
+    for (char c : text) {
+        if (isalpha(c)) {
+            int caseShift = isupper(c) ? 'A' : 'a';
+            c = (c - caseShift + shift) % 26 + caseShift;
+        }
+        encryptedString += c;
+    }
+    return encryptedString;
+}
+
+int main() {
+    int shift = 0;
+    string text;
+
+    cout << "Input text: ";
+    getline(cin, text);
+    cout << "Input shift: ";
+    cin >> shift;
+
+    string encrypted = encryptString(text, shift);
+    cout << "Result: " << encrypted << endl << endl;
+
+    system("pause");
+    return 0;
+}
